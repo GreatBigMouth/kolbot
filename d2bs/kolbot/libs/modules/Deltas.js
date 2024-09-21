e2856bd0a9105bd65b76ca066de5e1c3dc056e5e
GreatBigMouth <gbm780@gmail.com>
2024-09-22 00:21:29 +0200

    Added Delta.js modifications

--- d2bs/kolbot/libs/modules/Deltas.js
+++ d2bs/kolbot/libs/modules/Deltas.js
@@ -4,39 +4,39 @@
  *
  */
 (function (module, require) {
-	const Worker = require("Worker");
-	let instances = 0;
+  const Worker = require("Worker");
+  let instances = 0;
 
-	/** @constructor
+  /** @constructor
 	 * @class Delta */
-	module.exports = function (trackers) {
-		let active = true;
-		this.values = (Array.isArray(trackers) && (Array.isArray(trackers.first()) && trackers || [trackers])) || [];
-		this.track = function (id, checkerFn, callback) {
-			!id && (id = instances + 1);
-			return this.values.push({ id: id, active: true, fn: checkerFn, callback: callback, value: checkerFn() });
-		};
-		this.check = function () {
-			this.values.some(delta => {
-				if (delta.active) {
-					let val = delta.fn();
+  module.exports = function (trackers) {
+    let active = true;
+    this.values = (Array.isArray(trackers) && (Array.isArray(trackers.first()) && trackers || [trackers])) || [];
+    this.track = function (id, checkerFn, callback) {
+      !id && (id = instances + 1);
+      return this.values.push({ id: id, active: true, fn: checkerFn, callback: callback, value: checkerFn() });
+    };
+    this.check = function () {
+      this.values.some(delta => {
+        if (delta.active) {
+          let val = delta.fn();
 
-					if (delta.value !== val) {
-						let ret = delta.callback(delta.value, val);
-						delta.value = val;
+          if (delta.value !== val) {
+            let ret = delta.callback(delta.value, val);
+            delta.value = val;
 
-						return ret;
-					}
-				}
+            return ret;
+          }
+        }
 
-				return null;
-			});
-		};
+        return null;
+      });
+    };
 
-		this.toggle = (id) => id && (this.values.forEach(delta => (delta.id === id) && (delta.active = !delta.active)));
+    this.toggle = (id) => id && (this.values.forEach(delta => (delta.id === id) && (delta.active = !delta.active)));
 
-		//this.untrack = (id) => id && (this.values.splice(this.values.findIndex(delta => delta.id === id), 1));
-		/*this.untrack = (id) => {
+    //this.untrack = (id) => id && (this.values.splice(this.values.findIndex(delta => delta.id === id), 1));
+    /*this.untrack = (id) => {
 			if (id) {
 				let i = this.values.findIndex(delta => delta.id === id);
 				if (i === -1) return false;
@@ -45,16 +45,16 @@
 			return false;
 		};*/
 
-		this.untrack = (id) => this.values = this.values.filter(delta => delta.id !== id);
+    this.untrack = (id) => this.values = this.values.filter(delta => delta.id !== id);
 
-		this.destroy = () => active = false;
+    this.destroy = () => active = false;
 
-		Worker.runInBackground["__delta" + (instances++)] = () => active && (this.check() || true);
-		return this;
-	};
+    Worker.runInBackground["__delta" + (instances++)] = () => active && (this.check() || true);
+    return this;
+  };
 
 }).call(
-	null,
-	typeof module === "object" && module || {},
-	typeof require === "undefined" && (include("require.js") && require) || require
+  null,
+  typeof module === "object" && module || {},
+  typeof require === "undefined" && (include("require.js") && require) || require
 );
